02e689fb7ab20fcfbafb21a915447f3b11519522
faisal6699 <faisal.ahmed6699@gmail.com>
2020-11-07 18:44:47 +0600

    navbar edited

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
 
 const Header = () => {
+  const [shadow, setShadow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+       if (window.pageYOffset > 80 && window.pageYOffset < 689) {
+        setShadow(false);
+      } else {
+        setShadow(true);
+      }
+      //console.log(window.pageYOffset, shadow);
+    });
+  });
+
   return (
     <>
-      <Navbar collapseOnSelect expand="md" id="nav-header">
+      <Navbar
+        collapseOnSelect
+        expand="md"
+        id={shadow ? "floating-nav-header" : "nav-header"}
+      >
         <Navbar.Brand href="#home" className="text-title">
           LOGO
         </Navbar.Brand>
@@ -18,12 +35,10 @@ const Header = () => {
               <NavDropdown.Item href="#action/3.2">Chittagong</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Khulna</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#deets" className='login'>Log in</Nav.Link>
-            <Nav.Link
-              eventKey={2}
-              href="#memes"
-              className="add-submit"
-            >
+            <Nav.Link href="#deets" className="login">
+              Log in
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes" className="add-submit">
               Submit An Add
             </Nav.Link>
           </Nav>
