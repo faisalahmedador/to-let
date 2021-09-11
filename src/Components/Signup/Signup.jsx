8808c9e69c298615bbd9e8e7d775df48dd319f16
sharif  email = sharif.himu@v2.ltd <himu@DESKTOP-7G2SK1C.localdomain>
2021-09-11 21:30:29 +0600

    signup api integrate

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -3,44 +3,82 @@ import { Form, Col, Button, Overlay, Tooltip } from 'react-bootstrap';
 import { AiFillQuestionCircle } from 'react-icons/ai';
 import ReactDate from 'react-date-picker';
 import './Signup.scss';
+import { API } from '../../util/api';
+import { useHistory } from 'react-router-dom'
 
 const Signup = () => {
 
-    const [show, setShow] = useState(false);
-    const [ show2, setShow2 ] = useState(false)
-    const target = useRef(null);
-    const target2 = useRef(null);
-    const [value, onChange] = useState(new Date());
+    const [val, setVal ] = useState({first_name: '', last_name: '', number: '', pass: '' })
+    let history = useHistory()
 
+    console.log('val', val ) ;
+
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        console.log('e', val );
+        if(val.first_name != '' && val.last_name != '' && val.number != '' && val.pass != ''){
+
+            if(val.number.length === 11 ){
+
+            try{
+                let values = {
+                    first_name: val.first_name, 
+                    last_name: val.last_name , 
+                    number: val.number, 
+                    password: val.pass
+                }
+
+                const { data } = await API.post('/signup', values );
+                console.log('api data', data );
+                history.push('/')
+
+            }
+            catch(err){
+                alert(err?.response?.data?.message)
+            }
+
+
+    
+            }
+            else{
+                alert('Please give valid number of 11 digit, don\'t add +88 in front')
+            }
+
+        }
+        else{
+            alert('please fill all field')
+        }
+    }
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }} >
 
         <div className='signup-div' >
             <h1 style={{ color: '#715EF1' }} > Sign Up </h1>
-            <Form>
+            <Form onSubmit={ onSubmit } >
             <Form.Row style={{ marginBottom: '5px' }}>
                 <Col>
-                <Form.Control type='name' placeholder="First name" size='lg' />
+                <Form.Control type='name' placeholder="First name" size='lg' onChange={ (e) => setVal( {...val, first_name: e.target.value }) }  />
                 </Col>
                 <Col>
-                <Form.Control  type='name' placeholder="Last name" size='lg' />
+                <Form.Control  type='name' placeholder="Last name" size='lg' onChange={ (e) => setVal( {...val, last_name: e.target.value }) }  />
                 </Col>
             </Form.Row>
 
             <Form.Row  style={{ marginBottom: '5px' }} >
-                <Col>
-                    <Form.Control type="email" placeholder="Mobile Number/Email Address"  size='lg' />
+                <Col style={{ display: 'flex', justifyContent: 'center', alignItems: 'baseline' }} >
+                    <p style={{ fontWeight: 'bold', marginRight: '5px' }} > +88 </p>
+                    <Form.Control type="number" placeholder="Enter your 11 digit Mobile Number"  size='lg'  onChange={ (e) => setVal( {...val, number: e.target.value }) }  />
                 </Col>
             </Form.Row>
 
             <Form.Row style={{ marginBottom: '5px' }} >
                 <Col>
-                    <Form.Control type="password" placeholder="Password" size='lg' />
+                    <Form.Control type="password" placeholder="Password" size='lg' onChange={ (e) => setVal( {...val, pass: e.target.value }) } />
                 </Col>
             </Form.Row>
 
-            <div>
+            {/* <div>
             <p style={{ fontWeight: 'bold', color: '#715EF1', textAlign: 'left', display: 'flex', alignItems: 'center' }} > 
                 Date of Birth        
                 <Button  ref={target2} onClick={() => setShow2(!show2)} >
@@ -93,7 +131,7 @@ const Signup = () => {
                     <Form.Check inline label="Others" name="group1" type={'radio'} id={`inline-radio-3`} />
                     </div>
                 </Col>
-            </Form.Row>
+            </Form.Row> */}
 
             <Button variant="primary" className='submit-btn' type="submit">
                 Submit
