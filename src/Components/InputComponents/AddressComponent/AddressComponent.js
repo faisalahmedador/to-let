fc0baf0e88c30c8ecf2e0347406b19047dbaba9b
faisal6699 <faisal.ahmed6699@gmail.com>
2021-11-03 09:57:47 +0600

    form

--- src/Components/InputComponents/AddressComponent/AddressComponent.js
+++ src/Components/InputComponents/AddressComponent/AddressComponent.js
@@ -1,35 +1,57 @@
 import { set } from "js-cookie";
 import { React, useEffect, useState } from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
+import * as zilaList from '../../../Assets/locations/zilas.json';
+import * as divisionList from '../../../Assets/locations/divisions.json';
+import * as upazilaList from '../../../Assets/locations/upazilas.json';
+import * as unionList from '../../../Assets/locations/unions.json';
 
 
 const AddressComponent = ({ nextQuestion, id, setQuesSave }) => {
     console.log(nextQuestion);
-    const divisions = nextQuestion.dropOptionsDivision;
-    const [zilla, setZilla] = useState([])
-    const [upoZilla, setUpoZilla] = useState([])
+    const [divisions, setDivision] = useState([]);
+    const [zila, setZila] = useState([])
+    const [upaZila, setUpaZila] = useState([])
+    const [union, setUnion] = useState([]);
 
-    const [selectedDiv, setSelectedDiv] = useState({});
-    const [selectedZilla, setSelectedZilla] = useState({});
-    const [selectedUpoZilla, setSelectedUpoZilla] = useState({});
-    const [union, setUnion] = useState({});
-    const [road, setRoad] = useState({});
-    const [houseName, setHouseName] = useState({});
+    const [selectedDiv, setSelectedDiv] = useState();
+    const [selectedZila, setSelectedZila] = useState();
+    const [selectedUpaZila, setSelectedUpaZila] = useState();
+    const [selectedUnion, setSelectedUnion] = useState();
+    const [road, setRoad] = useState();
+    const [houseName, setHouseName] = useState();
 
     useEffect(() => {
-        if(selectedDiv && selectedZilla && houseName){
+        if(selectedDiv && selectedZila && houseName){
             let address = {
                 division: selectedDiv,
-                zilla: selectedUpoZilla,
-                upozilla: selectedUpoZilla,
-                union: union,
+                zila: selectedZila,
+                upazila: selectedUpaZila,
+                union: selectedUnion,
                 road: road,
                 houseName: houseName
             }
 
             setQuesSave(address);
         }
-    }, [houseName, road, selectedDiv, selectedUpoZilla, selectedZilla, setQuesSave, union])
+    }, [houseName, road, selectedDiv, selectedUpaZila, selectedZila, setQuesSave, union])
+
+    const setLocation = (locationInfo, type) => {
+        if (type === 'division') {
+            console.log(locationInfo)
+            setDivision(locationInfo.data)
+        } else if (type === 'zilas') {
+            setZila(locationInfo)
+        } else if (type === 'upazilas') {
+            setUpaZila(locationInfo.data);
+        } else if (type === 'union') {
+            setUnion(locationInfo.data)
+        }
+    }
+
+    useEffect(() => {
+        setLocation(divisionList[0], 'division');
+    }, [])
 
 
     return (
@@ -42,22 +64,17 @@ const AddressComponent = ({ nextQuestion, id, setQuesSave }) => {
                 title={'city'}
                 className='mb-3'
                 onChange={e => {
-                    let item = divisions.find(value => value.label === e.target.value);
-                    let zillaFinder = nextQuestion.dropOptionsZilla.find(zillas => zillas.id === item.id)
-                    console.log(zillaFinder);
-                    if (zillaFinder) {
-                        setZilla(zillaFinder.districts)
-                    }else{
-                        setZilla({})
-                    }
-                    setSelectedDiv(item ? item : {});
+                    let { id, name } = divisions.find(value => value.bn_name === e.target.value);
+                    setSelectedDiv(name);
+                    let zilaFinder = zilaList[0].data.filter(zilaItem => zilaItem.division_id === id)
+                    setZila(zilaFinder, 'zilas');
                 }}
             >
                 <option disabled selected hidden>division</option>
                 {
                     divisions.map(val => {
                         return (
-                            <option eventKey={val.id}  > {val.label} </option>
+                            <option id={val.id}  > {val.bn_name} </option>
                         )
                     })
                 }
@@ -72,21 +89,17 @@ const AddressComponent = ({ nextQuestion, id, setQuesSave }) => {
                 title={'city'}
                 className='mb-3'
                 onChange={e => {
-                    let item = zilla.find(value => value.label === e.target.value);
-                    let upoZillaFinder = nextQuestion.dropOptionsUpoZilla.find(upoZillas => upoZillas.SubId === item.SubId)
-                    if (upoZillaFinder) {
-                        setUpoZilla(upoZillaFinder.upozillas)
-                    }else{
-                        setUpoZilla({})
-                    }
-                    setSelectedZilla(item ? item : {});
+                    let {id, name} = zila.find(value => value.bn_name === e.target.value);
+                    setSelectedZila(name);
+                    let upaZilaFinder = upazilaList[0].data.filter(upazilaItem => upazilaItem.district_id === id);
+                    setUpaZila(upaZilaFinder, 'upazilas')
                 }}
             >
                 <option disabled selected hidden>Zilla</option>
                 {
-                    zilla.length && zilla.map(val => {
+                    zila.length && zila.map(val => {
                         return (
-                            <option eventKey={val.subId}  > {val.label} </option>
+                            <option id={val.id}  > {val.bn_name} </option>
                         )
                     })
                 }
@@ -101,29 +114,46 @@ const AddressComponent = ({ nextQuestion, id, setQuesSave }) => {
                 title={'city'}
                 className='mb-3'
                 onChange={e => {
-                    let item = upoZilla.find(value => value.label === e.target.value);
-                    setSelectedUpoZilla(item ? item : {});
+                    let {id, name} = upaZila.find(value => value.bn_name === e.target.value);
+                    setSelectedUpaZila(name);
+                    let unionFinder = unionList[0].data.filter(unionItem => unionItem.upazilla_id === id);
+                    console.log(unionFinder);
+                    setUnion(unionFinder);
                 }}
             >
                 <option disabled selected hidden>Upozilla</option>
                 {
-                    upoZilla.length && upoZilla.map(val => {
+                    upaZila.length && upaZila.map(val => {
                         return (
-                            <option eventKey={val.subIdUpo}  > {val.label} </option>
+                            <option id={id}  > {val.bn_name} </option>
                         )
                     })
                 }
 
             </Form.Control>
 
-            <Form.Group as={Row} className="mb-3" controlId="para">
-                <Form.Label column sm="4">
-                    Union/Block/Para/Ward
-                </Form.Label>
-                <Col sm="8">
-                    <Form.Control type='text' style={{ fontSize: '1.5rem', fontWeight: '600' }} id={id} onChange={(e) => setUnion(e.target.value)} />
-                </Col>
-            </Form.Group>
+            <Form.Control
+                as='select'
+                key={id}
+                id={`city`}
+                style={{ width: '100%' }}
+                title={'city'}
+                className='mb-3'
+                onChange={e => {
+                    let {id, name} = union.find(value => value.bn_name === e.target.value);
+                    setSelectedUnion(name);
+                }}
+            >
+                <option disabled selected hidden>Union</option>
+                {
+                    union.length && union.map(val => {
+                        return (
+                            <option id={id}  > {val.bn_name} </option>
+                        )
+                    })
+                }
+
+            </Form.Control>
 
             <Form.Group as={Row} className="mb-3" controlId="para">
                 <Form.Label column sm="4">
