d02bdc97ac23f14630819d8a1663e9c5fb8e5837
faisal6699 <faisal.ahmed6699@gmail.com>
2022-01-28 12:46:15 +0600

    advertise page started

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -2,7 +2,6 @@ import React from 'react'
 import UpperHeader from "../Components/UpperHeader/UpperHeader";
 import Header from "../Components/Header/Header";
 import LandingPage from "../Pages/LandingPage";
-import AdDetails from "../Pages/AdDetails";
 import Default from "../Pages/Default/Default";
 import { Switch, Route, Redirect, BrowserRouter  } from "react-router-dom";
 import Footer from "../Components/Footer/Footer";
@@ -12,6 +11,7 @@ import ProtectedRoute from './protectedRoute';
 import Login from '../Components/LogIn/LogIn';
 import Signup from "../Components/Signup/Signup";
 import ForgotPassword from "../Components/ForgotPassword/ForgotPassword";
+import advertisementDetails from '../Pages/advertisementDetails/advertisementDetails';
 
 const Routes = () => {
 
@@ -23,7 +23,7 @@ const Routes = () => {
       <>
         <Switch>
           <Route path="/" exact component={LandingPage} />
-          <Route path="/addetails" exact component={AdDetails} />
+          <Route path="/addetails" exact component={advertisementDetails} />
           <Route path='/searchresult' exact component={SearchResult} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/signup' component={Signup} />
